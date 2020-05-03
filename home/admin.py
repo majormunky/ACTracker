c1291fbf05df6189d9ecae0f9b122f00eb8d8f39
Josh Bright <josh.bright@gmail.com>
2020-05-02 20:04:34 -0700

    Added options to the fish admin page

--- home/admin.py
+++ home/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from home import models
 
 
-admin.site.register(models.Fish)
+class FishAdmin(admin.ModelAdmin):
+	list_display = ("name", "price", "location",)
+
+
+admin.site.register(models.Fish, FishAdmin)
 admin.site.register(models.Bug)
