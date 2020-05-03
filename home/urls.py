ef5a1b1f6c4f83e0253cfd4a3215620fd090db6a
Josh Bright <josh.bright@gmail.com>
2020-05-02 22:15:11 -0700

    Added detail pages

--- home/urls.py
+++ home/urls.py
@@ -5,5 +5,7 @@ from home import views
 urlpatterns = [
 	path('', views.index),
 	path('fish/', views.fish),
+	path('fish/<int:pk>/', views.fish_detail),
 	path('bugs/', views.bugs),
+	path('bugs/<int:pk>/', views.bug_detail),
 ]
--- home/views.py
+++ home/views.py
@@ -1,11 +1,12 @@
 import datetime
 from django.utils import timezone
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from home import models
 from home import utils
 
 
 def index(request):
+	min_price = request.GET.get("min_price", None)
 	now = timezone.now()
 	current_tz = timezone.get_current_timezone()
 	now = current_tz.normalize(now.astimezone(current_tz))
@@ -14,8 +15,12 @@ def index(request):
 	midnight = datetime.time(0)
 
 	fish_query = models.Fish.objects.filter(**month_filter)
+	if min_price:
+		fish_query = fish_query.filter(price__gt=min_price)
 	fish_list = utils.get_things_active_now(fish_query, now.time())
 	bug_query = models.Bug.objects.filter(**month_filter)
+	if min_price:
+		bug_query = bug_query.filter(price__gt=min_price)
 	bug_list = utils.get_things_active_now(bug_query, now.time())
 	return render(request, "home/index.html", {"fish_list": fish_list, "bug_list": bug_list, "now": now.time()})
 
@@ -28,3 +33,12 @@ def fish(request):
 def bugs(request):
 	bug_list = models.Bug.objects.all().order_by("name")
 	return render(request, "home/bugs.html", {"bug_list": bug_list})
+
+
+def fish_detail(request, pk):
+	fish_data = get_object_or_404(models.Fish, pk=pk)
+	return render(request, "home/fish_detail.html", {"fish_data": fish_data})
+
+def bug_detail(request, pk):
+	bug_data = get_object_or_404(models.Bug, pk=pk)
+	return render(request, "home/bug_detail.html", {"bug_data": bug_data})
