66094a45fd3295403449b5eb92c09d6a6030028a
Josh Bright <josh.bright@gmail.com>
2020-05-02 19:28:35 -0700

    Added a fish list page

--- home/views.py
+++ home/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from home import models
 
-# Create your views here.
+
 def index(request):
 	return render(request, "home/index.html", {})
+
+
+def fish(request):
+	fish_list = models.Fish.objects.all().order_by("name")
+	return render(request, "home/fish.html", {"fish_list": fish_list})
