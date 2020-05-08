2118fc38d6b3d943d4437e455b9d81747ff912e3
Josh Bright <josh.bright@gmail.com>
2020-05-08 14:23:47 -0700

    Added allowed hosts

--- ac_project/settings.py
+++ ac_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')%v2697p76e%q4$=lpehmikmanilz=tk9kzx)nch%1i6^5dbj$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.0.126", "127.0.0.1"]
 
 
 # Application definition
